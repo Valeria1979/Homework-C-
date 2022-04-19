2df64bee012f3c7a7de8fc90478bd1ed88e4af5e
Valeria Vodolazova <vvv91@internet.ru>
2022-04-19 16:05:55 +0300

    decided to task 6

---
+++ Seminar 1 task 6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine("Введите число: ");
+int namber = int.Parse(Console.ReadLine());
+
+if (namber % 2 ==0)
+
+{
+    Console.WriteLine($"число {namber} - четное число");
+}
+else
+
+{
+    Console.WriteLine($"число {namber} - нечетное число");
+}
